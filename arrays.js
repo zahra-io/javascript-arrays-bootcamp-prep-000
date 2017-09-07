849488419aab706f26fa64179169d20442691844
Zahra Khan <zarssskhan@gmail.com>
2017-09-07 06:27:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,3 +13,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
   return array;
 };
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
