4c05db3eef9e7dd6a24ee9113dfbf665209cc921
Zahra Khan <zarssskhan@gmail.com>
2017-09-07 06:34:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -41,4 +41,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array;
 };
 
-function removeElementFromEndOfArray(array)
+function removeElementFromEndOfArray(array) {
+  return array.slice(-1)
+}
