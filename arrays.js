46b4760edbd251958a4c3ad2700b3762fbb448e9
Zahra Khan <zarssskhan@gmail.com>
2017-09-07 06:27:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,5 +15,5 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 };
 
 function addElementToEndOfArray(array, element) {
-  return [...array, element]
-}
+  return [...array, element];
+};
