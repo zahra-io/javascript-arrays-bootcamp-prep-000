52027fd947079b634b243eed3f03d200ef5bb333
Zahra Khan <zarssskhan@gmail.com>
2017-09-07 06:31:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,4 +30,8 @@ function accessElementInArray(array, index) {
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift();
   return array;
+};
+
+function removeElementFromBeginningOfArray(array) {
+  
 }
